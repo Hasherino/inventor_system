e82b6954538a0fd80490172212ed3a69a31bc413
Hasherino <povilas.tamulis@gmail.com>
2021-12-21 09:32:44 +0200

    migration update

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->unsignedInteger('company_id');
+            $table->unsignedInteger('company_id')->nullable();
             $table->string('first_name');
             $table->string('last_name');
             $table->string('email')->unique();
