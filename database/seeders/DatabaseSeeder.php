7341be9540d547f57bfb24aae443f325a2a4a877
Hasherino <povilas.tamulis@gmail.com>
2021-12-31 11:27:23 +0200

    tweak

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,8 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\Company::factory(5)->create();
+//        \App\Models\Company::factory(5)->create();
         \App\Models\User::factory(1)->create();
-        \App\Models\Gear::factory(20)->create();
+//        \App\Models\Gear::factory(20)->create();
     }
 }
