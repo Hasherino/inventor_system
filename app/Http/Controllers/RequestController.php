2fe96b181b25cc0a8a871dc6fb45fb7666127858
Hasherino <povilas.tamulis@gmail.com>
2021-12-31 11:11:04 +0200

    bug fix

--- app/Http/Controllers/RequestController.php
+++ app/Http/Controllers/RequestController.php
@@ -40,7 +40,7 @@ class RequestController extends Controller
             $request['gear'] = $request->gear()->get();
             $request['lender_id'] = $request['gear']->first()->user_id;
         }
-        return $requests;
+        return $requests->values();
     }
 
     public function show($id) {
